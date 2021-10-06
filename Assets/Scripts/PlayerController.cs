6768e62941476cad2a136eb4886e6fc0f7c5c406
Pablo Vallejos <faktory.fm@live.com>
2021-10-06 16:42:08 -0300

    movement player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 movement;
+    private Animator _animator;
+    public float turnSpeed;
+    public Rigidbody _rigidbody;
+    private Quaternion rotation = Quaternion.identity;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();//refenciamos el animator
+        _rigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        movement.Set(horizontal,0,vertical);
+        
+        movement.Normalize();
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        
+        _animator.SetBool("IsWalking",isWalking);
+        
+        // creamos una variable de mirada deseada. 
+        Vector3 desiredForward=Vector3.RotateTowards(transform.forward,movement,turnSpeed*Time.deltaTime,0f);
+        
+         rotation=Quaternion.LookRotation(desiredForward);
+    }
+//cuando el animator cambie 
+    private void OnAnimatorMove()
+    {
+        _rigidbody.MovePosition(_rigidbody.position+movement*_animator.deltaPosition.magnitude);
+        _rigidbody.MoveRotation(rotation);
+       
+    }
+}
