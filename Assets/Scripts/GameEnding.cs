c905851de7e8cd2562e3f487d397fba685bb7b2a
Pablo Vallejos <faktory.fm@live.com>
2021-10-23 18:02:32 -0300

    modified:   Assets/Scenes/MainScene.unity

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -1,20 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
-    public float fadeDuration = 1f;
     
-    // Start is called before the first frame update
-    void Start()
+    public float fadeDuration = 1f;
+    private bool isPlayerAtExit;
+    public GameObject player;
+    public CanvasGroup exitBackGroupImageCanvasGroup;
+    
+    public float displayImageDuration = 1f;
+    
+    private float timer;
+
+    private void OnTriggerEnter(Collider other)
     {
+        if (other.gameObject==player)
+        {
+            isPlayerAtExit = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (isPlayerAtExit)
+        {
+            timer += Time.deltaTime;
+            //exitBackGroupImageCanvasGroup.alpha = timer / fadeDuration;
+           //lo mismo 
+           exitBackGroupImageCanvasGroup.alpha = Mathf.Clamp(timer / fadeDuration,0,1);
+           
+           if (timer>fadeDuration+displayImageDuration)
+            {
+                EndLevel();
+            }
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void EndLevel()
     {
-        
+       // SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Application.Quit();
     }
 }
