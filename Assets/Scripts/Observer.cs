f2e89a8978989de18cd02c291a81d445346d6d0d
Pablo Vallejos <faktory.fm@live.com>
2021-10-24 09:27:06 -0300

    Assets/Animation/Animators/Gargoyle.controller

--- Assets/Scripts/Observer.cs
+++ Assets/Scripts/Observer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,58 @@ using UnityEngine;
 [RequireComponent(typeof(CapsuleCollider))]
 public class Observer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //transform es un componente 
+    //vector 3  es un dato NO ES UN COMPORTAMIENTO
+
+    public Transform player;
+
+    private bool isPlayerInRange;
+    public GameEnding gameEnding;
+    
+    private void Update()
     {
-        
+        if (isPlayerInRange)
+        {
+            Vector3 direction = player.position - transform.position + Vector3.up;
+            Ray ray = new Ray(transform.position, direction);
+            
+            Debug.DrawRay(transform.position,direction,Color.yellow,Time.deltaTime,true);
+            
+            RaycastHit raycastHit;
+            if (Physics.Raycast(ray,out raycastHit))
+            {
+                if (raycastHit.collider.transform == player)
+                {
+                    
+                }
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform==player)
+        {
+            isPlayerInRange = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
     {
-        
+        if (other.transform==player)
+        {
+         
+            isPlayerInRange = false;   
+        }
     }
+    
+    // no se muestra cuando se compila el juego 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color=Color.red;
+        Gizmos.DrawSphere(transform.position,0.1f);
+        Gizmos.color=Color.blue;
+        Gizmos.DrawLine(transform.position,player.position+Vector3.up);
+    }
+
+    
 }
